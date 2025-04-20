e41bf303865bddb3a979da9cadf5dd207493a983
jofemodo <fernando@zynthian.org>
2025-04-20 12:15:33 +0200

    Add some log lines ...

--- ser2net/run.sh
+++ ser2net/run.sh
@@ -7,4 +7,7 @@ SERIAL_DEVICE=$(jq --raw-output ".serialDevice" $CONFIG_PATH)
 BAUD_RATE=$(jq --raw-output ".baudRate" $CONFIG_PATH)
 LISTEN_PORT=$(jq --raw-output ".port" $CONFIG_PATH)
 
-ser2net -C "${LISTEN_PORT}:raw:600:${SERIAL_DEVICE}:${BAUD_RATE} NONE 1STOPBIT 8DATABITS XONXOFF -RTSCTS"
+echo Initializing ...
+echo ser2net -C "${LISTEN_PORT}:raw:600:${SERIAL_DEVICE}:${BAUD_RATE} NONE 1STOPBIT 8DATABITS XONXOFF -RTSCTS" -c /etc/ser2net.conf
+
+ser2net -C "${LISTEN_PORT}:raw:600:${SERIAL_DEVICE}:${BAUD_RATE} NONE 1STOPBIT 8DATABITS XONXOFF -RTSCTS" -c /etc/ser2net.conf
